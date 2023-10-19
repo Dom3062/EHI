4dc92109993f48e175d1fd77b0779b2c0eee9194
Dom <38251670+Dom3062@users.noreply.github.com>
2023-10-19 02:45:11 +0200

    r190
    Menu: Fixed 4:3 aspect ratio

--- lua/EHIMenu.lua
+++ lua/EHIMenu.lua
@@ -24,8 +24,8 @@ EHIMenu = class()
 EHIMenu.make_fine_text = BlackMarketGui.make_fine_text
 function EHIMenu:init()
     local aspect_ratio = RenderSettings.resolution.x / RenderSettings.resolution.y
-    --local _1_33 = 4 / 3
-    if aspect_ratio == 1.6 then-- or aspect_ratio == _1_33 then -- 16:10 or 4:3
+    local _1_33 = 4 / 3
+    if aspect_ratio == 1.6 or aspect_ratio == _1_33 then -- 16:10 or 4:3
         self._ws = managers.gui_data:create_fullscreen_16_9_workspace()
         self._convert_mouse_pos = function(menu, x, y)
             return managers.mouse_pointer:convert_fullscreen_16_9_mouse_pos(x, y)
